a73df93acaf405a2c68ff0ad902109e4934d6fbd
Rich Salvucci <richsalvucci@mac.com>
2014-06-19 15:13:21 -0400

    fixed syntax error

--- app/models/handicap.rb
+++ app/models/handicap.rb
@@ -1,26 +1,27 @@
 class Handicap < ActiveRecord::Base
-  def calculate_handicap
-    @round = Round.find params[:round_id]
-    @user_round = UserRound.find params [:user_round_id] 
-    @course = Course.find params [:course_id]
+  # def calculate_handicap
+  #   @round = Round.find params[:round_id]
+  #   @user_round = UserRound.find params [:user_round_id] 
+  #   @course = Course.find params [:course_id]
 
 
-    @gross = @round.scores.sum(:score)
+  #   @gross = @round.scores.sum(:score)
 
     
-    if @user_round.tee == Back
-      @rating = @course.back_tee_rating
-      @slope = @course.back_tee_slope
-    elsif @user_round.tee == Middle
-      @rating = @course.middle_tee_rating
-      @slope = @course.middle_tee_slope
-    elsif @user_round.tee == Front
-      @rating = @course.front_tee_rating
-      @slope = @course.front_tee_slope
-    elsif @user_round.tee == Forward
-      @rating = @course.forward_tee_rating
-      @slope = @course.forward_tee_slope
+  #   if @user_round.tee == Back
+  #     @rating = @course.back_tee_rating
+  #     @slope = @course.back_tee_slope
+  #   elsif @user_round.tee == Middle
+  #     @rating = @course.middle_tee_rating
+  #     @slope = @course.middle_tee_slope
+  #   elsif @user_round.tee == Front
+  #     @rating = @course.front_tee_rating
+  #     @slope = @course.front_tee_slope
+  #   elsif @user_round.tee == Forward
+  #     @rating = @course.forward_tee_rating
+  #     @slope = @course.forward_tee_slope
 
-    @handicap_diff = (@gross - @rating) * 113/@slope
-  end
+  #   @handicap_diff = (@gross - @rating) * 113/@slope
+  # end
 end
+
